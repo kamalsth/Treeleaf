b15c611341f2ea2734f30b22b0bee760d766654c
Kamal Shrestha <kamal@lancemeup.com>
2023-11-01 14:43:35 +0545

    atm implementation project setup

---
+++ Q2_ATMImplementation-master/src/main/java/com/progressoft/induction/atm/Impl/ATMImpl.java
@@ -0,0 +1,25 @@
+package com.progressoft.induction.atm.Impl;
+
+import com.progressoft.induction.atm.ATM;
+import com.progressoft.induction.atm.Banknote;
+import com.progressoft.induction.atm.exceptions.AccountNotFoundException;
+import com.progressoft.induction.atm.exceptions.InsufficientFundsException;
+import com.progressoft.induction.atm.exceptions.NotEnoughMoneyInATMException;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ATMImpl implements ATM {
+    private final BankingSystemImpl bankingSystem=new BankingSystemImpl();
+    @Override
+    public List<Banknote> withdraw(String accountNumber, BigDecimal amount) {
+        // Your code here
+        return null;
+    }
+
+    @Override
+    public BigDecimal checkBalance(String accountNumber) {
+        return bankingSystem.getAccountBalance(accountNumber);
+    }
+}
---
+++ Q2_ATMImplementation-master/src/main/java/com/progressoft/induction/atm/Impl/BankingSystemImpl.java
@@ -0,0 +1,44 @@
+package com.progressoft.induction.atm.Impl;
+
+import com.progressoft.induction.atm.BankingSystem;
+import com.progressoft.induction.atm.Banknote;
+import com.progressoft.induction.atm.exceptions.InsufficientFundsException;
+import java.math.BigDecimal;
+import java.util.EnumMap;
+import java.util.HashMap;
+import java.util.Map;
+
+public class BankingSystemImpl implements BankingSystem {
+   Map<String, BigDecimal> accountBalanceMap = new HashMap<String, BigDecimal>();
+   EnumMap<Banknote,Integer> atmCashMap = new EnumMap<>(Banknote.class);
+
+    public BankingSystemImpl() {
+        atmCashMap.put(Banknote.FIFTY_JOD,10);
+        atmCashMap.put(Banknote.TWENTY_JOD,20);
+        atmCashMap.put(Banknote.TEN_JOD,100);
+        atmCashMap.put(Banknote.FIVE_JOD,100);
+
+        accountBalanceMap.put("123456789", BigDecimal.valueOf(1000.0));
+        accountBalanceMap.put("111111111", BigDecimal.valueOf(1000.0));
+        accountBalanceMap.put("222222222", BigDecimal.valueOf(1000.0));
+        accountBalanceMap.put("333333333", BigDecimal.valueOf(1000.0));
+        accountBalanceMap.put("444444444", BigDecimal.valueOf(1000.0));
+    }
+
+    public BigDecimal sumOfMoneyInAtm(){
+        // Your code
+        return null;
+    }
+
+
+    @Override
+    public BigDecimal getAccountBalance(String accountNumber){
+        //your code
+        return null;
+    }
+
+    @Override
+    public void debitAccount(String accountNumber, BigDecimal amount) {
+        //your code
+    }
+}

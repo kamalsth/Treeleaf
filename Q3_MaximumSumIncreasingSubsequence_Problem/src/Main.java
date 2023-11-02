47797b4904810fbdcec36b4643bd37ed3d182663
Kamal Shrestha <kamal@lancemeup.com>
2023-11-02 16:57:50 +0545

    Maximum sum increasing subsequence solved

---
+++ Q3_MaximumSumIncreasingSubsequence_Problem/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static List<Integer> maxSumIncreasingSubsequence(int[] sequence) {
+        int n = sequence.length;
+        int[] dp = new int[n];
+        int[] previousIndex = new int[n];
+
+        Arrays.fill(previousIndex, -1);
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = sequence[i];
+            for (int j = 0; j < i; j++) {
+                if (sequence[i] > sequence[j] && dp[i] < dp[j] + sequence[i]) {
+                    dp[i] = dp[j] + sequence[i];
+                    previousIndex[i] = j;
+                }
+            }
+        }
+
+        int maxSum = dp[0];
+        int maxSumIndex = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (dp[i] > maxSum) {
+                maxSum = dp[i];
+                maxSumIndex = i;
+            }
+        }
+
+        List<Integer> subsequence = new ArrayList<>();
+        while (maxSumIndex != -1) {
+            subsequence.add(sequence[maxSumIndex]);
+            maxSumIndex = previousIndex[maxSumIndex];
+        }
+
+        Collections.sort(subsequence);
+        return subsequence;
+    }
+    public static void main(String[] args) {
+        int[] sequence = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11};
+        List<Integer> result = maxSumIncreasingSubsequence(sequence);
+        System.out.println("Maximum Sum Increasing Subsequence: " + result);    }
+}
